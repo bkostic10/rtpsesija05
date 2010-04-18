540e68b5d1596bd2deab81eb04c43e5a74bbc0cc
Baxter <Baxter@xpwindows7.sbb.rs>
2010-04-18 22:09:06 +0200

    Problem

---
+++ Klase/Operacije.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Klase
+{
+    public class Operacije
+    {
+        private char _operacija;
+        private double _b;
+        private double _a;
+
+        private Operacije(double a, double b, char operacija)
+        {
+            _a = a;
+            _b = b;
+            _operacija = operacija;
+        }
+        public double racunaj()
+        {
+            if (_operacija == '+')
+                return _a + _b;
+            if (_operacija == '-')
+                return _a - _b;
+            if (_operacija == '*')
+                return _a * _b;
+            else return _a / _b;
+        }
+        public static Operacije sabiranje(double a, double b)
+        {
+            return new Operacije(a, b, '+');
+        }
+        public static Operacije oduzimanje(double a, double b)
+        {
+            return new Operacije(a, b, '-');
+        }
+        public static Operacije mnozenje(double a, double b)
+        {
+            return new Operacije(a, b, '*');
+        }
+        public static Operacije deljenje(double a, double b)
+        {
+            return new Operacije(a, b, '/');
+        }
+    }
+}
---
+++ Main/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Klase;
+namespace Main
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double a, b;
+            while (true)
+            {
+                Console.WriteLine("Unesi prvi broj:");
+                try
+                {
+                    a = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Morate uneti broj");
+                }
+
+            }
+            while (true)
+            {
+                Console.WriteLine("Unesi drugi broj:");
+                try
+                {
+                    b = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Morate uneti broj");
+                }
+            }
+            Operacije s = Operacije.sabiranje(a, b);
+            Operacije o = Operacije.oduzimanje(a, b);
+            Operacije m = Operacije.mnozenje(a, b);
+            Operacije d = Operacije.deljenje(a, b);
+            Console.WriteLine("Zbir: {0}", s.racunaj());
+            Console.WriteLine("Razlika: {0}", o.racunaj());
+            Console.WriteLine("Proizvod: {0}", m.racunaj());
+            Console.WriteLine("Kolicnik: {0}", d.racunaj());
+        }
+    }
+}
